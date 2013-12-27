10ca5f435c79efb7ebbd02067629899301208304
Jeff Stephens <jefftheman45@gmail.com>
2013-12-27 13:44:48 -0700

    blank out constant files

--- android/TriviaCast/src/com/jeffstephens/triviacast/TVCConstants.java
+++ android/TriviaCast/src/com/jeffstephens/triviacast/TVCConstants.java
@@ -1,6 +1,6 @@
 package com.jeffstephens.triviacast;
 
-class TVCConstants{
-	public static final String APP_NAME = "1f96e9a0-9cf0-4e61-910e-c76f33bd42a2";
-	public static final String GAME_NAMESPACE = "com.bears.triviaCast";
+public class TVCConstants{
+	public static final String APP_NAME = "";
+	public static final String GAME_NAMESPACE = "";
 }
