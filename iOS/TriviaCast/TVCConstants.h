d5e10a21d12ef70195d5aaa98819bdd8480e317a
John Brophy <jbrophy@wustl.edu>
2013-12-27 14:03:13 -0500

    Created a constants file for App ID and Namespace

---
+++ iOS/TriviaCast/TVCConstants.h
@@ -0,0 +1,13 @@
+//
+//  TVCConstants.h
+//  TriviaCast
+//
+//  Created by John Brophy on 12/27/13.
+//  Copyright (c) 2013 smokeHau5. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const kNamespace;// = @"com.bears.triviaCast";
+extern NSString * const kReceiverApplicationName;// = @"1f96e9a0-9cf0-4e61-910e-c76f33bd42a2";
+
