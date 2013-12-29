1d4909e2af154bde9997b5f9e009815f9891bed6
Jeff Stephens <jefftheman45@gmail.com>
2013-12-29 12:38:31 -0700

    add moderation system for prompts

--- receiver/db.php
+++ receiver/db.php
@@ -3,7 +3,7 @@ require_once("backend_constants.php");
 
 define("DB_TABLE", "prompts");
 
-$valid_commands = array("get", "put");
+$valid_commands = array("get", "put", "approve");
 $response = array();
 $type = $_GET['type'];
 
@@ -25,6 +25,7 @@ if($db->connect_errno){
 if($type == 'put'){
 	$text = trim(htmlentities($db->real_escape_string($_POST['text'])));
 	$author = trim(htmlentities($db->real_escape_string($_POST['author'])));
+	$auth_key = md5(time() . $text);
 
 	if(!strlen($author) || strlen($author) > 255){
 		$author = 'Anonymous';
@@ -39,9 +40,17 @@ if($type == 'put'){
 	// check for dupes
 	//$dupe_query = "SELECT `ID` FROM `" . DB_TABLE . "` WHERE LOWER(`text`) = LOWER('$text');";
 
-	$query = "INSERT INTO `" . DB_TABLE . "` (`text`, `author`, `time`, `ip`) VALUES ('$text', '$author', " . time() . ", '" . $_SERVER['REMOTE_ADDR'] . "');";
+	$query = "INSERT INTO `" . DB_TABLE . "` (`text`, `author`, `time`, `ip`, `auth_key`) VALUES ('$text', '$author', " . time() . ", '" . $_SERVER['REMOTE_ADDR'] . "', '$auth_key');";
 
 	if($db->query($query) === TRUE){
+		$email_body = "Good news! A new TriviaCast prompt was added by $author from {$_SERVER['REMOTE_ADDR']} (" . gethostbyaddr($_SERVER['REMOTE_ADDR']) . ").\n\n\"$text\"\n\n";
+		$email_body .= "Approve this prompt by clicking this link: http://trivia-cast.com/db.php?type=approve&auth=" . $auth_key;
+
+		$approvers = array("jefftheman45@gmail.com", "brophs@gmail.com");
+		foreach($approvers as $email){
+			mail($email, "New TriviaCast Prompt Added", $email_body);
+		}
+
 		$response['type'] = 'success';
 		$response['message'] = 'Your prompt was successfully added to TriviaCast.';
 		die(json_encode($response));
@@ -62,10 +71,10 @@ else if($type == 'get'){
 
 	$limitString = '';
 	if(isset($_GET['notID'])){
-		$limitString = " WHERE `ID` != " . $db->real_escape_string($_GET['notID']);
+		$limitString = " AND `ID` != " . $db->real_escape_string($_GET['notID']);
 	}
 
-	$query = "SELECT `ID`, `text`, `author` FROM `" . DB_TABLE . "`$limitString;";
+	$query = "SELECT `ID`, `text`, `author` FROM `" . DB_TABLE . "` WHERE `auth_key` = ''$limitString;";
 
 	if($result = $db->query($query)){
 		$responses = array();
@@ -89,6 +98,34 @@ else if($type == 'get'){
 		$response['type'] = 'error';
 		$response['message'] = 'A database error occurred: ' . $db->error;
 		die(json_encode($response));
+	}
+}
+
+else if($type == 'approve'){
+	$auth_key = $db->real_escape_string($_GET['auth']);
+	$query = "UPDATE `" . DB_TABLE . "` SET `auth_key` = '' WHERE `auth_key` = '$auth_key' LIMIT 1;";
+
+	if($result = $db->query($query)){
+	?><!doctype html>
+<html>
+<head>
+	<title>TriviaCast Admin</title>
+</head>
+<body>
+<?php
+
+		if($db->affected_rows > 0){
+			echo '<h1>Prompt approved.</h1>';
+		}
+		else{
+			echo '<h1>Invalid request or already approved.</h1>';
+		}
+	?>
+</body>
+</html><?php
+	}
+	else{
+		die("Database error: " . $db->error);
 	}
 }
 
